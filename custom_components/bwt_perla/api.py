9ca34f19fe7e8e3b8d20ede7c616421cc935b502
aschamberger <mail@andreass.net>
2021-12-28 23:27:50 +0100

    extend get_data() with regeneratingagent value and add get_warnings() + get_holiday_mode() + save_value()

--- custom_components/bwt_perla/api.py
+++ custom_components/bwt_perla/api.py
@@ -2,6 +2,7 @@ import logging
 
 import requests
 import urllib3
+from bs4 import BeautifulSoup
 
 from .const import DOMAIN
 
@@ -50,10 +51,110 @@ class BWTPerlaApi:
         _LOGGER.debug(f"{DOMAIN} - actualizesignals response {response.text}")
         signal_response: dict = response.json()
 
+        # regeneratingagent request
+        url = self.base_url + "info/regeneratingagent"
+        response = session.post(url, headers=self.headers, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - regeneratingagent response {response.text}")
+        soup = BeautifulSoup(response.content, "html.parser")
+        results = soup.find_all("span", class_="subMenuValue")
+        keys = ['regeneratingagent_commissioning', 'regeneratingagent_maintenance', 'regeneratingagent_refill', 'regenerations_total', 'regenerations_maintenance']
+        values = []
+        for result in results:
+            values.append(result.text.split(" ")[0])
+        regeneratingagent_response = dict(zip(keys, values))
+        
         # logout
         url = self.base_url + "users/logout"
         response = session.get(url, verify=False)
 
-        merged_response = data_response | signal_response
+        merged_response = data_response | signal_response | regeneratingagent_response
         _LOGGER.debug(f"{DOMAIN} - merged_response {merged_response}")
         return merged_response
+
+    def get_warnings(self) -> dict:
+        # create a session with login page
+        url = self.base_url + "users/login"
+        session = requests.Session()
+        response = session.get(url, verify=False)
+
+        # login with password
+        url = self.base_url + "users/login"
+        data = {"_method": "POST", "STLoginPWField": self.code, "function": "save"}
+        response = session.post(url, headers=self.headers, data=data, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - login response {response.text}")
+
+        # warnings request
+        url = self.base_url + "home/warnings"
+        response = session.post(url, headers=self.headers, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - warnings response {response.text}")
+        soup = BeautifulSoup(response.content, "html.parser")
+        results = soup.find_all("div", class_="notification")
+        warnings_response = {}
+        for result in results:
+            divs = result.find("div", class_="notificationLeftText").find_all("div")
+            warnings_response[result['id']] = divs[0].text+" "+divs[1].text
+            
+        # logout
+        url = self.base_url + "users/logout"
+        response = session.get(url, verify=False)
+
+        return warnings_response    
+        
+    def get_holiday_mode(self) -> bool:
+        # create a session with login page
+        url = self.base_url + "users/login"
+        session = requests.Session()
+        response = session.get(url, verify=False)
+
+        # login with password
+        url = self.base_url + "users/login"
+        data = {"_method": "POST", "STLoginPWField": self.code, "function": "save"}
+        response = session.post(url, headers=self.headers, data=data, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - login response {response.text}")
+
+        # holidaymode request
+        url = self.base_url + "functions/holidaymode"
+        response = session.post(url, headers=self.headers, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - holidaymode response {response.text}")
+        soup = BeautifulSoup(response.content, "html.parser")
+        results = soup.find(id="changeHolidaymode")
+        holidayMode = False
+        if results.has_attr('checked'):
+            holidayMode = True
+                
+        # logout
+        url = self.base_url + "users/logout"
+        response = session.get(url, verify=False)
+
+        return holidayMode    
+        
+    # Extra Regeneration auslösen --> 482, 1
+    # Extra Spülung auslösen --> 483, 1
+    # Urlaubsmodus einschalten --> 484, 1
+    # Urlaubsmodus ausschalten --> 484, 0
+    def save_value(self, id: int, value: int) -> bool:
+        # create a session with login page
+        url = self.base_url + "users/login"
+        session = requests.Session()
+        response = session.get(url, verify=False)
+
+        # login with password
+        url = self.base_url + "users/login"
+        data = {"_method": "POST", "STLoginPWField": self.code, "function": "save"}
+        response = session.post(url, headers=self.headers, data=data, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - login response {response.text}")
+
+        # set value for given id
+        url = self.base_url + "keyboard/saveValue"
+        data = {"_method": "POST", "ID": id, "Value": value}
+        response = session.post(url, headers=self.headers, data=data, verify=False)
+        _LOGGER.debug(f"{DOMAIN} - saveValue response {response.text}")
+        success = False
+        if (response.status_code == requests.codes.ok):
+            success = True   
+        
+        # logout
+        url = self.base_url + "users/logout"
+        response = session.get(url, verify=False)
+        
+        return success
